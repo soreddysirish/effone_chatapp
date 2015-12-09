c274ea6c511318de480a1c8511350c534d8cb50e
soreddysirish <ssireeshreddy@gmail.com>
2015-12-09 16:44:27 +0530

    added last_seen_time to user controller in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :last_seen_at, if: proc { user_signed_in?}
+  private
+
+  def last_seen_at
+    current_user.update_attribute(:last_sign_in_at, Time.now)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+     :trackable, :validatable
+     validates_uniqueness_of  :user_name
+     validates_presence_of :user_name
 end
